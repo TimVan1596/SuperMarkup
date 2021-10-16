5a208926be058a762fc0fd8b89949ed05ed99683
TimVan <877020296@qq.com>
2021-10-17 01:46:41 +0800

    update 1.支持手机操作 2.实现单条多标注 3.实现自动提示关键词

--- js/label.js
+++ js/label.js
@@ -14,15 +14,21 @@ var origin_array = [];
 var fix_array = [];
 var utilTool = new Util();
 
+var keywords = ['是一款', '开发', '设计', '研制', '订购', '国空军', '国海军', '国陆军'
+    , '采购', '第二次世界大战', '二战', '搭载', '战场上', '战争中', '参与过', '可以发射', '设计'
+    , '是一款', '是一个', '一战', '第一次世界大战', '改进', '装备']
+
 
 //注意：导航 依赖 element 模块，否则无法进行功能性操作
 layui.use('element', function () {
     var element = layui.element;
 
+    // 初始化
+    $(".fix_text").val('');
+    $(".show_fixed").val('');
+
 
     $("#uploadFromTextBtn").click(function () {
-
-
         // 审核备注
         layer.prompt({
             formType: 2,
@@ -34,7 +40,6 @@ layui.use('element', function () {
             analysis(value)
             layer.close(index);
             return value;
-
         });
     });
 
@@ -42,20 +47,21 @@ layui.use('element', function () {
     $(".choose_file").click(function (event) {
         $("#files").click();
         $("#files").change(function () {
+            $(".fix_text").val('');
             $(".show_fixed").val('');
             $("#file_list").empty();
             var files_input = $("#files");
             var file_length = files_input[0].files.length;
             var items = files_input[0].files;
             var fragment = "";
-
+            var fileName = "";
             if (file_length > 0) {
                 for (var i = 0; i < file_length; i++) {
-                    var file_name = items[i].name;
-                    fragment += "<li>" + file_name + "</li>"
+                    fileName = items[i].name;
+                    fragment += "<li>" + fileName + "</li>"
                 }
             }
-            $("#file_list").append(fragment);
+            $("#card-title").text(fileName)
 
             // 读取内容
             let selectedFile = document.getElementById('files').files[0];
@@ -72,12 +78,12 @@ layui.use('element', function () {
     });
 
     // 下一个
-    $(".next_btn").click(function () {
+    $("#next-btn").click(function () {
         // 读取修改的句子
         var fix_text = $(".fix_text").val();
         fix_text = utilTool.clearString(fix_text);
         if (fix_text.length == 0) {
-            var fix = "0 " + $(".origin_text").val();
+            var fix = "0 " + $("#origin-text").text();
             fix_text = fix;
         }
         fix_text = utilTool.clearString(fix_text);
@@ -90,7 +96,7 @@ layui.use('element', function () {
         origin_array.shift();
         // 判断是否还有
         if (origin_array.length > 0) {
-            $(".origin_text").val(origin_array[0]);
+            $("#origin-text").text(origin_array[0]);
         } else {
             alert("换下一个吧hxd");
             var fixed_text = $(".show_fixed").val();
@@ -101,20 +107,33 @@ layui.use('element', function () {
             location.reload();
         }
         $(".fix_text").val("");
+
+        // textarea自动换行
+        var obj = document.getElementById("show-fixed-ta");
+        obj.scrollTop = obj.scrollHeight;
+
     });
 
-    // 解析显示
+    // 解析并显示
     function analysis(text) {
         origin_array = text.split('。');
         for (var i = 0; i < origin_array.length; i++) {
             origin_array[i] = utilTool.clearString(origin_array[i]);
         }
-        $(".origin_text").val(origin_array[0]);
+        var firstText = origin_array[0];
+        // firstText = '<span style="color: #0000FF">' + firstText + '</span>';
+        firstText = textHighlight(firstText, keywords);
+        $("#origin-text").html(firstText);
     }
 
-    $(".option").click(function () {
-        var origin = $(".origin_text").val();
-        fixOriginText(origin, $(this.name))
+    $(".options-btn").click(function () {
+        var origin = $("#origin-text").text();
+        if (origin.length > 0) {
+            fixOriginText(origin, $(this.name))
+        } else {
+            layer.msg('未选择文本')
+        }
+
     });
 
     // 修改
@@ -140,9 +159,12 @@ layui.use('element', function () {
                 fix_sentence = "9 " + origin + "。";
                 break;
             default:
-                console.log("出错了哦");
+                layer.msg("出错了哦");
+
         }
-        $(".fix_text").val(fix_sentence);
+        var originVal = $(".fix_text").val();
+
+        $(".fix_text").val(originVal + fix_sentence + '\n');
     }
 
     // 展示
@@ -152,17 +174,14 @@ layui.use('element', function () {
         $(".show_fixed").val(fixed_title);
     }
 
-    //从文本读取并展示
-    function readAndShow(text) {
-        // 读取内容
-        let selectedFile = document.getElementById('files').files[0];
-        let reader = new FileReader(); // 读取TXT起重要作用的
-        reader.readAsText(selectedFile);
-        reader.onload = oFREvent => { // 读取完毕从中取值
-            let pointsTxt = oFREvent.target.result;
-            analysis(pointsTxt);
-        };
+    //文字中符合关键词的高亮
+    function textHighlight(text, keywords) {
+        var len = keywords.length
+        for (var i = 0; i < len; i++) {
+            var keyword = keywords[i]
+            text = text.replace(keyword, "<span style='color: #1E9FFF'>" + keyword + "</span>")
+        }
+        return text;
     }
-
 
 });
