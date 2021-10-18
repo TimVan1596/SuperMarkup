eb868b3fc2719546127bea3b4019c33fa157fe2b
TimVan <877020296@qq.com>
2021-10-18 15:32:24 +0800

    update:1、修复标注后无法生成新文件 2、固定文本框的高度 3、增加关键词的提示高亮

--- js/label.js
+++ js/label.js
@@ -9,15 +9,23 @@ class Util {
     }
 }
 
-// 全局变量
+/** 全局变量
+ *  origin_array = 从源文件按照句号分割的全部段落
+ *  fixArray
+ * */
 var origin_array = [];
-var fix_array = [];
+var fixArray = [];
 var utilTool = new Util();
+var fileName = ""
 
 var keywords = ['是一款', '开发', '设计', '使用国家', '使用国', '研制', '订购', '国空军', '国海军'
     , '国陆军', '生产国家', '生产国', '拥有国家', '拥有国', '开发公司', '研发公司', '产地', '制造国'
     , '采购', '第二次世界大战', '二战', '搭载', '战场上', '战争中', '参与过', '可以发射', '设计'
-    , '是一款', '是一个', '一战', '第一次世界大战', '改进', '装备', '是一', '使用', '产地']
+    , '是一款', '是一个', '一战', '第一次世界大战', '改进', '装备', '是一', '使用', '产地'
+    , '战场上', '战争中', '参与过', '战役', '属于', '服役', '配属']
+var regexs = [/(是|属于|服役)([\u4E00-\uFA29]|[\uE7C7-\uE7F3]|[a-zA-Z0-9_])*(舰|艇|机|船)/g, /驻([\u4E00-\uFA29]|[\uE7C7-\uE7F3]|[a-zA-Z0-9_])*(部队|军队|海军|陆军|空军)/g, /采购([\u4E00-\uFA29]|[\uE7C7-\uE7F3]|[a-zA-Z0-9_])*(架|台|量)/g
+    , /由([\u4E00-\uFA29]|[\uE7C7-\uE7F3]|[a-zA-Z0-9_])*(研制|研发|设计|装备)/g, /(是|属于|服役)([\u4E00-\uFA29]|[\uE7C7-\uE7F3]|[a-zA-Z0-9_])*的/g, /在([\u4E00-\uFA29]|[\uE7C7-\uE7F3]|[a-zA-Z0-9_])*(战场|战争|大战|战|战役)/g];
+keywords = regexs.concat(keywords)
 
 
 layui.config({
@@ -74,7 +82,7 @@ layui.use(['element', 'layer', 'ClipboardJS'], function () {
             var file_length = files_input[0].files.length;
             var items = files_input[0].files;
             var fragment = "";
-            var fileName = "";
+            fileName = "";
             if (file_length > 0) {
                 for (var i = 0; i < file_length; i++) {
                     fileName = items[i].name;
@@ -100,38 +108,71 @@ layui.use(['element', 'layer', 'ClipboardJS'], function () {
     // 下一个
     $("#next-btn").click(function () {
         // 读取修改的句子
-        var fix_text = $(".fix_text").val();
-        fix_text = utilTool.clearString(fix_text);
-        if (fix_text.length == 0) {
-            var fix = "0 " + $("#origin-text").text();
-            fix_text = fix;
+        var fixText = $(".fix_text").val();
+        var originText = $("#origin-text").text();
+        console.log('origin_array')
+        if (origin_array.length <= 0) {
+            //询问框
+            layer.confirm('换下一个吧hxd？', {
+                btn: ['好的', '再改改'] //按钮
+            }, function () {
+                var fixed_text = $(".show_fixed").val();
+                var blob = new Blob([fixed_text], {
+                    type: "text/plain;charset=utf-8"
+                });
+                var newFileName = getFileName(fileName) + '_new' + getExtension(fileName)
+                saveAs(blob, newFileName);
+                location.reload();
+            }, function () {
+            });
+            return
+        } else {
+
         }
-        fix_text = utilTool.clearString(fix_text);
-        if (fix_text.length > 0 && (fix_text != '0')) // 需要加别的么。。。算了吧
-            fix_array.push(fix_text);
-        // 展示已经修改的
-        showFixed();
+        fixText = utilTool.clearString(fixText);
+        // 如果没有进行修改默认为0（未选择实例、操作员等）
+        if (fixText.length <= 0) {
+            fixText = "0 " + originText;
+        }
+
+        // 将fix框中的内容放入结果框
+        var showTex = $(".show_fixed").val();
+        showTex += fixText + "\n";
+        $(".show_fixed").val(showTex);
 
         // 删除原数组第一个元素
         origin_array.shift();
+
+        console.log(origin_array)
+
+
         // 判断是否还有
         if (origin_array.length > 0) {
-            $("#origin-text").text(origin_array[0]);
+            var firstText = origin_array[0];
+            firstText = textHighlight(firstText, keywords);
+            $("#origin-text").html(firstText);
+
         } else {
-            alert("换下一个吧hxd");
-            var fixed_text = $(".show_fixed").val();
-            var blob = new Blob([fixed_text], {
-                type: "text/plain;charset=utf-8"
+            //询问框
+            layer.confirm('换下一个吧hxd？', {
+                btn: ['好的', '再改改'] //按钮
+            }, function () {
+                var fixed_text = $(".show_fixed").val();
+                var blob = new Blob([fixed_text], {
+                    type: "text/plain;charset=utf-8"
+                });
+                var newFileName = getFileName(fileName) + '_new' + getExtension(fileName)
+                saveAs(blob, newFileName);
+                location.reload();
+            }, function () {
             });
-            saveAs(blob, file_name);
-            location.reload();
+
         }
         $(".fix_text").val("");
 
         // textarea自动换行
         var obj = document.getElementById("show-fixed-ta");
         obj.scrollTop = obj.scrollHeight;
-
     });
 
     // 解析并显示
@@ -140,6 +181,9 @@ layui.use(['element', 'layer', 'ClipboardJS'], function () {
         for (var i = 0; i < origin_array.length; i++) {
             origin_array[i] = utilTool.clearString(origin_array[i]);
         }
+        cleanEmpty(origin_array)
+
+
         var firstText = origin_array[0];
         // firstText = '<span style="color: #0000FF">' + firstText + '</span>';
         firstText = textHighlight(firstText, keywords);
@@ -190,7 +234,7 @@ layui.use(['element', 'layer', 'ClipboardJS'], function () {
     // 展示
     function showFixed() {
         var fixed_title = $(".show_fixed").val();
-        fixed_title += fix_array[fix_array.length - 1] + "\n";
+        fixed_title += fixArray[fixArray.length - 1] + "\n";
         $(".show_fixed").val(fixed_title);
     }
 
@@ -199,7 +243,18 @@ layui.use(['element', 'layer', 'ClipboardJS'], function () {
         var len = keywords.length
         for (var i = 0; i < len; i++) {
             var keyword = keywords[i]
-            text = text.replace(keyword, "<span style='color: #1E9FFF'>" + keyword + "</span>")
+            var match = text.match(keyword)
+            //匹配多处的情况
+            if (match instanceof Array) {
+                var matchLen = match.length
+                for (var j = 0; j < matchLen; j++) {
+                    var elem = match[j]
+                    text = text.replace(elem, "<span style='color: #1E9FFF'>" + elem + "</span>")
+                }
+            } else {
+                text = text.replace(match, "<span style='color: #1E9FFF'>" + match + "</span>")
+            }
+
         }
         return text;
     }
@@ -216,7 +271,25 @@ layui.use(['element', 'layer', 'ClipboardJS'], function () {
         });
     })
 
-    //复制到剪切板
+    // 获取文件名
+    function getFileName(name) {
+        return name.substring(0, name.lastIndexOf("."))
+    }
 
+    // 获取 .后缀名
+    function getExtension(name) {
+        return name.substring(name.lastIndexOf("."))
+    }
 });
 
+
+//清空数组中的空数组元素如[[],{a:1}]
+function cleanEmpty(array) {
+    for (var i = 0; i < array.length; i++) {
+        if ((array[i] instanceof Array || typeof (array[i]) == 'string') && array[i].length === 0) {
+            array.splice(i, 1);//返回指定的元素
+            i--;
+        }
+    }
+    return this;
+}
